366a160858965cd507b168a61d6026ac5ae1e0cc
Apar Shrestha <67278825+ares-1889@users.noreply.github.com>
2024-11-17 20:48:29 +0545

    Update RecordList.jsx

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -41,7 +41,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://localhost:5050/record/`);
+      const response = await fetch(`https://backend-rnel.onrender.com/record/record/`);
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         console.error(message);
