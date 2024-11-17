1739949250d28d88209e13c2a2c6261812738879
Apar Shrestha <67278825+ares-1889@users.noreply.github.com>
2024-11-17 20:35:08 +0545

    Update RecordList.jsx

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -56,7 +56,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`http://localhost:5050/record/${id}`, {
+    await fetch(`https://backend-rnel.onrender.com/record/${id}`, {
       method: "DELETE",
     });
     const newRecords = records.filter((el) => el._id !== id);
